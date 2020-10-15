eee4cef3f0839628bca681334481c43d7a843994
Drumato <drumatech109@gmail.com>
2020-10-15 12:47:23 +0900

    fix

--- src/file/elf64.rs
+++ src/file/elf64.rs
@@ -23,19 +23,22 @@ impl ELF64 {
         // .shstrtab を追加する場合,先にヘッダを変更する必要がある.
         let is_section_name_table = sct.name == ".shstrtab";
 
-        let mut name_bytes = sct.name.as_bytes().to_vec();
-        let name_length = name_bytes.len() as u64;
         self.sections.push(sct);
 
         if is_section_name_table {
             self.ehdr.e_shstrndx = self.sections.len() as u16 - 1;
         }
 
-        if let Some(ref mut sct_name_table) = self.sections[self.ehdr.e_shstrndx as usize].bytes {
-            sct_name_table.push(0x00);
-            sct_name_table.append(&mut name_bytes);
-            self.sections[self.ehdr.e_shstrndx as usize].header.sh_size += name_length + 1;
+        if self.sections.len() == 1 {
+            return;
         }
+
+        let prev_sct_name_len = self.sections[self.sections.len() - 2].name.as_bytes().len() as u32;
+        let prev_sct_name_offset = self.sections[self.sections.len() - 2].header.sh_name;
+
+        let appended_section = self.sections.len() - 1;
+        self.sections[appended_section].header.sh_name =
+            prev_sct_name_offset + prev_sct_name_len + 1;
     }
 
     /// get section index if predicate returns true.
