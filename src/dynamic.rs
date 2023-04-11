3e615c8db96c667eb2014c7909847122501f3838
Drumato <drumato43@gmail.com>
2023-04-11 17:45:08 +0900

    refactor: remove all codes!!!!!!
    Signed-off-by: Drumato <drumato43@gmail.com>

--- src/dynamic.rs
+++
@@ -1,8 +0,0 @@
-mod elf64;
-pub use elf64::*;
-mod elf32;
-pub use elf32::*;
-mod entry_type;
-pub use entry_type::*;
-mod flags;
-pub use flags::*;
--- src/file.rs
+++
@@ -1,8 +0,0 @@
-#[allow(unused_imports)]
-pub use base::*;
-pub use elf32::*;
-pub use elf64::*;
-
-mod base;
-mod elf32;
-mod elf64;
